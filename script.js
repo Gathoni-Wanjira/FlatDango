b46cf1f303d971b3442ec14d29609c4c6bfe3b70
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-02 15:05:31 +0300

    FetchMoviesById

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-const allMovies = document.querySelector("#all-movies") 
+const allMovies = document.querySelector("#all-movies ul") 
 
 function fetchMovies () {
     fetch (" http://localhost:3000/films")
     .then(res => res.json())
     .then(movies => {
         console.log(movies);
+        displayAllMovies(movies)
     })
+  
 }
 fetchMovies()
+ 
+function displayAllMovies (movies){
+    
+    movies.forEach(movie => {
+        // Create Dynamic Nodes
+    const list = document.createElement('li'); 
+
+    // Assign Nodes 
+        list.textContent = movie.title
+        list.id = movie.id
+        list.addEventListener('click', seeMovieDetails)
+        allMovies.appendChild(list)
+    });
+}
+
+function seeMovieDetails (event){
+    fetchMoviesbyId(event.target.id)
+   
+}
+function fetchMoviesbyId (id){
+    fetch (`http://localhost:3000/films/${id}`)
+    .then(res => res.json())
+    .then(movie => {
+        console.log(movie);
+    
+    })
+
+}
+
+
