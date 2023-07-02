eadef88781e5e70189c82fe398c51e3839566f69
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-02 16:30:52 +0300

    Available Tickets

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const allMovies = document.querySelector("#all-movies ul") 
+const allMovies = document.querySelector("#all-movies ul")
 
-function fetchMovies () {
-    fetch (" http://localhost:3000/films")
-    .then(res => res.json())
-    .then(movies => {
-        console.log(movies);
-        displayAllMovies(movies)
-    })
-  
+function fetchMovies() {
+    fetch(" http://localhost:3000/films")
+        .then(res => res.json())
+        .then(movies => {
+            console.log(movies);
+            displayAllMovies(movies)
+        })
+
 }
 fetchMovies()
- 
-function displayAllMovies (movies){
-    
+
+function displayAllMovies(movies) {
+
     movies.forEach(movie => {
         // Create Dynamic Nodes
-    const list = document.createElement('li'); 
+        const list = document.createElement('li');
 
-    // Assign Nodes 
+        // Assign Nodes 
         list.textContent = movie.title
         list.id = movie.id
         list.addEventListener('click', seeMovieDetails)
@@ -25,38 +25,43 @@ function displayAllMovies (movies){
     });
 }
 
-function seeMovieDetails (event){
+function seeMovieDetails(event) {
     fetchMoviesbyId(event.target.id)
-   
-}
-function fetchMoviesbyId (id){
-    fetch (`http://localhost:3000/films/${id}`)
-    .then(res => res.json())
-    .then(movie => {
-        console.log(movie);
-        renderMovieDetails(movie)
-    
-    })
 
 }
-function renderMovieDetails (movie){
+function fetchMoviesbyId(id) {
+    fetch(`http://localhost:3000/films/${id}`)
+        .then(res => res.json())
+        .then(movie => {
+            console.log(movie);
+            renderMovieDetails(movie)
+
+        })
+
+}
+function renderMovieDetails(movie) {
     // Dynamic nodes 
-    
+
     const movieTitle = document.querySelector("#Movie-Title")
     const movieImage = document.querySelector("#movie-image")
     const movieDescription = document.querySelector("#movie-description")
 
     const movieRuntime = document.querySelector("#movie-runtime span")
     const movieShowtime = document.querySelector("#movie-showtime span")
+    const availableTickets = document.querySelector("#available-tickets")
     const movieBuyTicket = document.querySelector("#BuyTicket-btn")
     const removeBuyTicket = document.querySelector("#RemoveTicket-btn")
-    
-movieTitle.textContent = movie.title
-movieImage.src = movie.poster
-movieDescription.textContent = movie.description
+
+    movieTitle.textContent = movie.title
+    movieImage.src = movie.poster
+    movieDescription.textContent = movie.description
+
+    movieRuntime.textContent = movie.runtime
+    movieShowtime.textContent = movie.showtime
+
+    availableTickets.textContent = movie.capacity - movie.tickets_sold
 
 
-     
 
 
 }
